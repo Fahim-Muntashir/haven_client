a78de171874064a3d103f586c25b4d9222b6b152
FahimMuntashir1 <fahimasdf07@gmail.com>
2024-05-04 11:39:22 +0600

    Added all

--- src/app/components/Home/BuySellBg.tsx
+++ src/app/components/Home/BuySellBg.tsx
@@ -8,10 +8,10 @@ const BuySellBg = () => {
                 <div className="absolute w-full h-full top-0 left-0 bg-cover bg-center bg-no-repeat opacity- bg-fixed" style={{ backgroundImage: 'url(https://d1pnnwteuly8z3.cloudfront.net/images/dafc1e05-b0e8-4c6d-b375-4a62333bbd5a/71fe09e2-9320-4f61-8eca-067243e2ef68.jpeg)' }}></div>
                 <div className="text-white display-2 text-center relative">
                     <h1><span className='text-4xl'>The Haven House </span></h1>
-                    <div class="flex justify-center mx-auto mt-2 mb-3">
-                        <span class="inline-block w-40 h-1 bg-blue-400 rounded-full"></span>
-                        <span class="inline-block w-3 h-1 mx-1 bg-blue-500 rounded-full"></span>
-                        <span class="inline-block w-1 h-1 bg-blue-400 rounded-full"></span>
+                    <div className="flex justify-center mx-auto mt-2 mb-3">
+                        <span className="inline-block w-40 h-1 bg-blue-400 rounded-full"></span>
+                        <span className="inline-block w-3 h-1 mx-1 bg-blue-500 rounded-full"></span>
+                        <span className="inline-block w-1 h-1 bg-blue-400 rounded-full"></span>
                     </div>
                     <h1 className='text-4xl font-semibold text-gray-100 lg:w-3/6 mx-auto'>Your Premier Real Estate Agency for Buying or Selling Your Dream Property.</h1>
                     <div className='flex gap-4 items-center justify-center mt-4'>
--- src/app/components/Home/ClientTestimonial.tsx
+++ src/app/components/Home/ClientTestimonial.tsx
@@ -35,14 +35,14 @@ const ClientTestimonial = () => {
 
     return (
         <div className="my-20 w-4/5 mx-auto">
-            <h1 class="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl dark:text-white">
+            <h1 className="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl dark:text-white">
                 What clients saying
             </h1>
 
-            <div class="flex justify-center mx-auto mt-6">
-                <span class="inline-block w-40 h-1 bg-blue-500 rounded-full"></span>
-                <span class="inline-block w-3 h-1 mx-1 bg-blue-500 rounded-full"></span>
-                <span class="inline-block w-1 h-1 bg-blue-500 rounded-full"></span>
+            <div className="flex justify-center mx-auto mt-6">
+                <span className="inline-block w-40 h-1 bg-blue-500 rounded-full"></span>
+                <span className="inline-block w-3 h-1 mx-1 bg-blue-500 rounded-full"></span>
+                <span className="inline-block w-1 h-1 bg-blue-500 rounded-full"></span>
             </div>
             <Swiper
                 spaceBetween={30}
@@ -63,19 +63,19 @@ const ClientTestimonial = () => {
                     ClientTestimonialData.map((property, index) => (
                         <SwiperSlide>
                             <div className=' my-10'>
-                                <p class="flex items-center text-center text-gray-500 lg:mx-8">
+                                <p className="flex items-center text-center text-gray-500 lg:mx-8">
                                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, quam. Odio voluptatem officiis
                                     eos illo! Pariatur, totam alias. Beatae accusamus earum quos obcaecati minima molestias. Possimus
                                     minima dolores itaque! Esse! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea voluptates
                                     fugiat corrupti
                                 </p>
 
-                                <div class="flex flex-col items-center justify-center mt-8">
-                                    <img class="object-cover rounded-full w-14 h-14" src="https://images.unsplash.com/photo-1499470932971-a90681ce8530?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" />
+                                <div className="flex flex-col items-center justify-center mt-8">
+                                    <img className="object-cover rounded-full w-14 h-14" src="https://images.unsplash.com/photo-1499470932971-a90681ce8530?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" />
 
-                                    <div class="mt-4 text-center">
-                                        <h1 class="font-semibold text-gray-800 dark:text-white">Mia Brown</h1>
-                                        <span class="text-sm text-gray-500 dark:text-gray-400">Marketer</span>
+                                    <div className="mt-4 text-center">
+                                        <h1 className="font-semibold text-gray-800 dark:text-white">Mia Brown</h1>
+                                        <span className="text-sm text-gray-500 dark:text-gray-400">Marketer</span>
                                     </div>
                                 </div>
                             </div>
